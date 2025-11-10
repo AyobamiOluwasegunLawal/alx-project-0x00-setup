89ff510efc38f4c3cc7340bbc3fafa4e4ae8b011
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-11-10 15:58:21 +0100

    done

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -8,6 +8,8 @@ const Landing: React.FC = () => {
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
       <Button title="Click me" styles="w-lg rounded-lg"/>
+      <Button title="Click me" styles="w-lg rounded-sm"/>
+      <Button title="Click me" styles="w-lg rounded-full"/>
     </div>
   );
 };
